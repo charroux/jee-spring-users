879273bc01164aa5772c9dd0170b67afbca1b8b1
charroux <benoit.charroux@gmail.com>
2023-01-19 14:42:36 +0100

    first commit

---
+++ src/main/java/com/example/carServiceUser/CarServiceUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.carServiceUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carServiceUser/CarServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carServiceUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
